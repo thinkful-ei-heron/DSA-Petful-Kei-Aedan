2397643e8772edf87a354ed66d97901afda6e74c
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-20 16:38:42 +0000

    second commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  PORT : process.env.PORT || 8080,
+  //added node environment
+  NODE_ENV: process.env.NODE_ENV || 'development',
+};
---
+++ src/server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const cors = require('cors');
+//aded helmet, morgan
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { NODE_ENV } = require('./config');
+const app = express();
+
+const cats = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy 2',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy 3',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+];
+const dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus 2',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus 3',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+];
+// const DOGS = require('./dogs.json');
+
+//added morganoption
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+app.use(morgan(morganOption));
+app.use(helmet());
+
+app.use(cors());
+
+app.get('/api/cats', (req, res) => {
+  let response = cats;
+  return res.status(200).json(response);
+});
+
+app.delete('/api/cats', (req, res) => {
+  let adopted = cats[0];
+  cats.splice(0, 1);
+  return res.status(204).json(adopted);
+});
+
+app.get('/api/dogs', (req, res) => {
+  let response = dogs;
+  return res.status(200).json(response);
+});
+
+app.delete('/api/dogs', (req, res) => {
+  let adopted = dogs[0];
+  cats.splice(0, 1);
+  return res.status(204).json(adopted);
+});
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+app.listen(8080,()=>{
+  console.log('Serving on 8080');
+});
+
+module.exports = app;
