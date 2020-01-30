01c84b561c880e9a90460a485286d184a9c0e603
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-01-30 02:15:01 -0500

    minor fix

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ const dogs = [
   {
     imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
     imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus 2',
+    name: 'Zog 2',
     sex: 'Male',
     age: 3,
     breed: 'Golden Retriever',
